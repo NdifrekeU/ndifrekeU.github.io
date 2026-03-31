e98cb5d03c4d45bfdc1c653ad28fa6165eabd56b
Ndifreke Umoh <62891191+NdifrekeU@users.noreply.github.com>
2026-03-31 21:25:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Portfolio Loaded");
+// autoplay videos on hover
+document.querySelectorAll('.game-video').forEach(video => {
+    video.addEventListener('mouseenter', () => video.play());
+    video.addEventListener('mouseleave', () => video.pause());
+});
